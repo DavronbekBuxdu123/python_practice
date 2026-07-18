e8e8fd912ddbeacc0d614c00cd514d095338f4f2
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-07-19 00:03:49 +0500

    variables practice

---
+++ variables/variables.py
@@ -0,0 +1,46 @@
+#Strings
+first_name="Davron"
+food="pizza"
+email="dava123@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is:  {email}")
+
+#Integer
+age=21
+quantity=3
+num_of_students=30
+
+print(f"I'm {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+#Float
+price=10.99
+gpa=4.5
+distance=5.5
+
+print(f"The price is ${price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance} km")
+
+#Boolean
+is_student=True
+for_sale=False
+is_online=True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You aren't student")    
+
+if for_sale:
+    print("That item is for sale")    
+else:
+    print("That item isn't for sale")    
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")        
