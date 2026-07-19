6ed5c8bb582847c44593b1402c54f45ac0aa3550
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-07-19 17:52:52 +0500

    practice conditional expressions

---
+++ conditional_expressions/conditional.py
@@ -0,0 +1,16 @@
+# Exercise 1
+num=5
+a=6
+b=7
+age=13
+is_admin=True
+odd_or_even="Even" if num%2==0 else "Odd"
+max_num=a if a > b else b
+min_num=a if a < b else b
+old_or_young= "old" if age > 18 else "young"
+access="Full Access" if is_admin else "Limited Access"
+
+
+
+
+print(odd_or_even,max_num,min_num,old_or_young,access)
