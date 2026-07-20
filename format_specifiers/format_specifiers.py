b12b3cd5578e1fb18d8bb91087c119c572f7b74b
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-07-20 13:21:25 +0500

    practice format_specifiers

---
+++ format_specifiers/format_specifiers.py
@@ -0,0 +1,31 @@
+price1=3.14898
+price2=-987.65
+price3=12.356
+name="DAVA"
+a=5
+b=-5
+c=10000000000
+
+print(f"The price is ${price1:.2f}")
+print(f"The price is ${price2:.2f}")
+print(f"The price is ${price3:.2f}")
+
+
+print(f"The price is ${price1:.3f}")
+print(f"The price is ${price2:.1f}")
+print(f"The price is ${price3:.2f}")
+
+
+print(f"The price is ${price3:10}")
+print(f"The price is ${price3:010}")
+
+
+print(f"{name:<10}")
+print(f"{name:>10}")
+print(f"{name:^10}")
+
+
+print(f"{a: }")
+print(f"{b: }")
+print(f"{a:+}")
+print(f"{c:,}")
