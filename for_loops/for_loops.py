d2a38b20a607d958877812e181031a7f78c208fd
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-07-20 15:40:59 +0500

    practice for loops

---
+++ for_loops/for_loops.py
@@ -0,0 +1,25 @@
+# Exercise 1
+for x in reversed(range(1,10)):
+    print(x)
+
+
+# Exercise 2
+credit_card="1122-3344-5566-7788"
+for x in credit_card:
+    print(x)
+
+
+# Exercise 3
+for x in range(1,30):
+    if x==13:
+        continue
+    else:
+        print(x)
+
+
+# Exercise 4
+for x in range(1,20):
+    if x==13:
+        break
+    else:
+        print(x)
